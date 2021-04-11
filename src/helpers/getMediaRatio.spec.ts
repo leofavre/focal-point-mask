8b6c508ecb9a7a9ff14f89685ce42285680b6e62
Leonardo Favre <leo@leofavre.com>
2021-04-10 23:13:47 -0300

    chore: implement tests

--- src/helpers/getMediaRatio.spec.ts
+++ src/helpers/getMediaRatio.spec.ts
@@ -1,15 +1,12 @@
 import getMediaRatio from './getMediaRatio';
-import type { MediaElement } from '../types/MediaElement';
-
-let mediaElement: MediaElement;
 
 describe('getMediaRatio', () => {
   it('should use naturalWidth and naturalHeight if media is an image', () => {
-    mediaElement = {
+    const mediaElement = {
       nodeName: 'IMG',
       naturalWidth: 150,
       naturalHeight: 200
-    } as HTMLImageElement;
+    } as unknown as HTMLImageElement;
 
     expect(getMediaRatio(mediaElement)).toBe(150 / 200);
   });
@@ -19,50 +16,50 @@ describe('getMediaRatio', () => {
   });
 
   it('should use videoWidth and videoHeight if media is a video', () => {
-    mediaElement = {
+    const mediaElement = {
       nodeName: 'VIDEO',
       videoWidth: 150,
       videoHeight: 200
-    } as HTMLVideoElement;
+    } as unknown as HTMLVideoElement;
 
     expect(getMediaRatio(mediaElement)).toBe(150 / 200);
   });
 
   it('should return undefined if the element is not a media element', () => {
-    mediaElement = {
+    const mediaElement = {
       naturalWidth: 150,
       naturalHeight: 200
-    } as HTMLImageElement;
+    } as unknown as HTMLImageElement;
 
     expect(getMediaRatio(mediaElement)).toBeUndefined();
   });
 
   it('should return undefined if ratio is Infinity', () => {
-    mediaElement = {
+    const mediaElement = {
       nodeName: 'IMG',
       naturalWidth: 150,
       naturalHeight: 0
-    } as HTMLImageElement;
+    } as unknown as HTMLImageElement;
 
     expect(getMediaRatio(mediaElement)).toBeUndefined();
   });
 
   it('should return undefined if ratio is -Infinity', () => {
-    mediaElement = {
+    const mediaElement = {
       nodeName: 'IMG',
       naturalWidth: 150,
       naturalHeight: -0
-    } as HTMLImageElement;
+    } as unknown as HTMLImageElement;
 
     expect(getMediaRatio(mediaElement)).toBeUndefined();
   });
 
   it('should return undefined if ratio is NaN', () => {
-    mediaElement = {
+    const mediaElement = {
       nodeName: 'IMG',
       naturalWidth: 0,
       naturalHeight: 0
-    } as HTMLImageElement;
+    } as unknown as HTMLImageElement;
 
     expect(getMediaRatio(mediaElement)).toBeUndefined();
   });
