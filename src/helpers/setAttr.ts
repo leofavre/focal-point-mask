999cbae900e8706049c88417de20e951b37ef228
Leonardo Favre <leo@leofavre.com>
2021-04-13 22:58:36 -0300

    chore: improve setAttr

--- src/helpers/setAttr.ts
+++ src/helpers/setAttr.ts
@@ -1,6 +1,7 @@
 function setAttr<A> (el: HTMLElement, attr: A, value: unknown): void {
-  if (value != null) {
-    el.setAttribute(String(attr), String(value));
+  if (value != null && value !== false) {
+    const parsedValue = value === true ? '' : String(value);
+    el.setAttribute(String(attr), parsedValue);
   } else {
     el.removeAttribute(String(attr));
   }
