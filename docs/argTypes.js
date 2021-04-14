95ad1173e82c6458e8b2189eca0df8ed640ff9a6
Leonardo Favre <leo@leofavre.com>
2021-04-14 19:03:23 -0300

    chore: move the ArgsTable to the Docs panel

--- docs/argTypes.js
+++ docs/argTypes.js
@@ -8,11 +8,11 @@ export const argTypes = {
       'The coordinate of the media area that should be preserved when it is cropped by the mask.\n\n' +
       'Similarly to **background-position** in CSS, it expects a pair of position keywords or percentage values representing the distance from the media top left corner to the focal point.\n\n' +
       'If a single value is passed, the second value is set to *"center"*.\n\n' +
-      'Examples: `top right` `center left` `25% 65%`',
+      'Examples: `"top right"` `"center left"` `"25% 65%"`',
     table: {
       category: 'Properties',
       type: { summary: 'string' },
-      defaultValue: { summary: 'center' }
+      defaultValue: { summary: '"center"' }
     }
   },
   mediaRatio: {
@@ -20,7 +20,7 @@ export const argTypes = {
     description: '' +
       '**Media width divided by its height.**\n\n' +
       'Although the ratio is calculated while the media loads, setting this property is useful to prevent it from resizing during the process.\n\n' +
-      'Examples: `16/9` `4/3`',
+      'Examples: `"16/9"` `"4/3"`',
     table: {
       category: 'Properties',
       type: { summary: 'string' }
