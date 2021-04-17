b458054f07391ecfc474d53a0803bdc1af82af9c
Leonardo Favre <leo@leofavre.com>
2021-04-17 17:30:41 -0300

    chore: implement showOverlay in a poor way (must be better)

--- docs/examples/index.js
+++
@@ -1,7 +0,0 @@
-import Lake from './Lake';
-import Street from './Street';
-
-export {
-  Lake,
-  Street
-};
--- docs/helpers/Example.tsx
+++ docs/helpers/Example.tsx
@@ -1,4 +1,19 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useRef, useState, useEffect } from 'react';
+import type FocalPointMask from '../../src/class';
+
+interface OverlayStyle {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+}
+
+const initialOverlayStyle: OverlayStyle = {
+  top: 0,
+  left: 0,
+  width: 0,
+  height: 0
+};
 
 interface ExampleProps {
   name: string;
@@ -15,15 +30,46 @@ const Example: FC<ExampleProps> = ({
   duration = 4300,
   showOverlay
 }: ExampleProps) => {
-  const [count, setCount] = useState<number>(-1);
+  const [count, setCount] = useState<number>(0);
   const [imgSrcLoaded, setImgSrcLoaded] = useState<boolean>(false);
   const [focalPoint, setFocalPoint] = useState<string>(focalPoints[0]);
+  const [overlayStyle, setOverlayStyle] =
+    useState<OverlayStyle>(initialOverlayStyle);
+  const maskElementRef = useRef();
+
+  console.log(focalPoint);
 
   const handleLoad = () => setImgSrcLoaded(true);
 
   const handleAnimation = () => {
     setCount(count + 1);
   };
+
+  const handleMaskResize = () => {
+    const maskElement: FocalPointMask | undefined = maskElementRef.current;
+
+    if (maskElement != null && showOverlay) {
+      const targetElement = maskElement.children[0];
+      const targetPos = targetElement.getBoundingClientRect();
+
+      const nextOverlayStyle = {
+        top: targetPos.top,
+        left: targetPos.left,
+        width: targetPos.width,
+        height: targetPos.height
+      };
+
+      setOverlayStyle(nextOverlayStyle);
+    }
+  };
+
+  useEffect(() => {
+    const maskElement: FocalPointMask | undefined = maskElementRef.current;
+
+    if (maskElement != null && maskElement.onMaskResize == null) {
+      maskElement.onMaskResize = handleMaskResize;
+    }
+  }, []);
 
   useEffect(() => {
     const len = focalPoints.length;
@@ -34,6 +80,7 @@ const Example: FC<ExampleProps> = ({
   return (
     <div className={`example example-${name}`}>
       <focal-point-mask
+        ref={maskElementRef}
         focalPoint={focalPoint}
         onAnimationStart={handleAnimation}
         onAnimationIteration={handleAnimation}
@@ -45,8 +92,17 @@ const Example: FC<ExampleProps> = ({
           onLoad={handleLoad}
         />
       </focal-point-mask>
-      <div className={`overlay ${showOverlay ? 'visible' : ''}`}>
-        <span className="focal-point"/>
+      <div
+        className={`overlay ${showOverlay ? 'visible' : ''}`}
+        style={overlayStyle}
+      >
+        <span
+          className="focal-point"
+          style={{
+            top: focalPoint.split(' ')[0],
+            left: focalPoint.split(' ')[1]
+          }}
+        />
       </div>
     </div>
   );
